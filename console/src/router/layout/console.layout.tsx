5394d9014c38867dc0e37e80f905e96f1dfe8334
planchon <paul@planchon.io>
2024-06-12 23:09:08 +0200

    feat: console bg

--- console/src/router/layout/console.layout.tsx
+++ console/src/router/layout/console.layout.tsx
@@ -1,9 +1,14 @@
+import bg from "@/assets/bg.svg";
 import { AsideMenu } from "@/components/menu/aside.menu";
 import { Outlet } from "react-router-dom";
 
 export const ConsoleLayout = () => {
   return (
-    <div className="relative m-0 flex h-screen w-screen justify-between bg-gray-100 p-0">
+    <div
+      className="relative m-0 flex h-screen w-screen justify-between bg-gray-50 bg-cover bg-fixed bg-no-repeat p-0"
+      style={{
+        backgroundImage: `url(${bg})`,
+      }}>
       <div className="flex h-full w-full flex-row">
         <div className="h-full w-[300px]">
           <AsideMenu />
