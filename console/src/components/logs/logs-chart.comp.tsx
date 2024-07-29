e98ee761145511035c8103e646e4929fb753e878
planchon <paul@planchon.io>
2024-07-30 00:40:59 +0200

    feat: log chart

---
+++ console/src/components/logs/logs-chart.comp.tsx
@@ -0,0 +1,97 @@
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Info",
+    color: "hsl(var(--blue-400))",
+  },
+  mobile: {
+    label: "Error",
+    color: "hsl(var(--red-400))",
+  },
+} satisfies ChartConfig;
+
+export function LogsChart() {
+  return (
+    <div className="">
+      <ChartContainer config={chartConfig}>
+        <BarChart accessibilityLayer data={chartData} height={150} width={100}>
+          <CartesianGrid vertical={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <Bar dataKey="desktop" stackId="a" fill="#2563eb" radius={[0, 0, 4, 4]} />
+          <Bar dataKey="mobile" stackId="a" fill="#ef4444" radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ChartContainer>
+    </div>
+  );
+}
--- console/src/pages/service/logs.tsx
+++ console/src/pages/service/logs.tsx
@@ -1,9 +1,34 @@
+import { LogsChart } from "@/components/logs/logs-chart.comp";
 import { LogsRender } from "@/components/logs/logs.comp";
+
+const LogsHeader = () => {
+  return (
+    <div
+      className="border-b py-1 pl-2 text-sm font-normal text-gray-400"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "auto minmax(0px, 120px) minmax(0px, 120px) minmax(0px, 200px) minmax(0px, 1fr)",
+        gridAutoRows: "auto",
+      }}>
+      <div>
+        <div className="scrollbar-hide h-full w-full min-w-[0px] cursor-default select-none truncate pr-2 text-xs">
+          <a className="block h-full w-2 rounded-[3px]"></a>
+        </div>
+      </div>
+      <p>Date</p>
+      <p>User</p>
+      <p>Repo</p>
+      <p>Message</p>
+    </div>
+  );
+};
 
 export const LogsPage = () => {
   return (
-    <>
+    <div className="flex h-full flex-col">
+      <LogsChart />
+      <LogsHeader />
       <LogsRender />
-    </>
+    </div>
   );
 };
