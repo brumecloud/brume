f5fb1386680c319aefa00088de84ab8f1b62989f
planchon <paul@planchon.io>
2025-08-19 00:53:58 +0200

    frontend is more type safe now

--- console/src/components/menu/user/user.menu.tsx
+++ console/src/components/menu/user/user.menu.tsx
@@ -1,33 +1,32 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar";
-import { USER_FRAGMENT } from "@/gql/user.graphql";
-import { useFragment } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ME_QUERY } from "@/gql/user.graphql";
 
 export const UserMenu = () => {
-  const { data: me, complete } = useFragment({
-    fragment: USER_FRAGMENT,
-    from: "me",
-  });
+	const { data: me, loading } = useQuery(ME_QUERY);
 
-  if (!complete) {
-    return (
-      <div className="flex animate-pulse select-none flex-row items-center gap-x-3 transition-all">
-        <div className="h-[30px] w-[30px] rounded-full bg-gray-200" />
-        <div className="h-[12px] w-[130px] rounded-full bg-gray-200" />
-      </div>
-    );
-  }
+	if (loading) {
+		return (
+			<div className="flex animate-pulse select-none flex-row items-center gap-x-3 transition-all">
+				<div className="h-[30px] w-[30px] rounded-full bg-gray-200" />
+				<div className="h-[12px] w-[130px] rounded-full bg-gray-200" />
+			</div>
+		);
+	}
 
-  return (
-    <div className="flex select-none flex-row items-center gap-x-3">
-      <Avatar className="h-8 w-8">
-        <AvatarImage src={me.avatar} />
-        <AvatarFallback>{me.name.slice(0, 2)}</AvatarFallback>
-      </Avatar>
-      <span className="text-sm text-gray-800">{me.name}</span>
-    </div>
-  );
+	if (!me?.me) {
+		return null;
+	}
+
+	const user = me.me;
+
+	return (
+		<div className="flex select-none flex-row items-center gap-x-3">
+			<Avatar className="h-8 w-8">
+				<AvatarImage src={user.avatar} />
+				<AvatarFallback>{user.name.slice(0, 2)}</AvatarFallback>
+			</Avatar>
+			<span className="text-sm text-gray-800">{user.name}</span>
+		</div>
+	);
 };
