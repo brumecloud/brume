1dd3fbca83a7bc95159cf2f799bb0e7c678f7e8b
planchon <paul@planchon.io>
2025-08-21 23:01:45 +0200

    move the redirect

--- console/src/router/layout/console.layout.tsx
+++ console/src/router/layout/console.layout.tsx
@@ -1,8 +1,18 @@
 import bg from "@/assets/bg.svg";
 import { AsideMenu } from "@/components/menu/aside.menu";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export const ConsoleLayout = () => {
+  const location = useLocation();
+  const nav = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      nav("/overview", { replace: true });
+    }
+  }, [location]);
+
   return (
     <div
       className="relative m-0 flex h-screen w-screen justify-between bg-gray-50 bg-cover bg-fixed bg-no-repeat p-0"
