3860d06e84a4c70d8ab50a24df0067da8df7218c
planchon <paul@planchon.io>
2025-08-21 21:11:36 +0200

    work on the adding a new stack part

--- console/src/components/stepper/step-item.tsx
+++ console/src/components/stepper/step-item.tsx
@@ -1,116 +1,135 @@
+import { cn } from "@/utils";
 import { AnimatePresence, motion } from "motion/react";
 import type React from "react";
-import { cn } from "@/utils";
+
 import { useStep, useStepItem } from "./step-body";
 
 export const StepItem = ({
-	children,
-	className,
-	...props
+  children,
+  className,
+  ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-	const { step: currentStep } = useStep();
-	const { step } = useStepItem();
-	const isValidated = currentStep > step;
+  const { step: currentStep } = useStep();
+  const { step } = useStepItem();
+  const isValidated = currentStep > step;
 
-	return (
-		<div
-			className={cn(
-				"flex flex-col gap-4 transition-all duration-300 relative pb-8 pl-4",
-				className,
-			)}
-			{...props}
-		>
-			<div className="absolute left-[-1px] top-0 h-full w-[1px] bg-gray-300" />
-			{isValidated && (
-				<motion.div
-					initial={{ height: 0 }}
-					animate={{ height: "100%" }}
-					exit={{ height: 0 }}
-					transition={{ duration: 0.5 }}
-					className="absolute left-[-1px] top-0 w-[1px] bg-green-500"
-				/>
-			)}
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={cn(
+        "relative flex flex-col gap-4 pb-8 pl-4 transition-all duration-300",
+        className
+      )}
+      {...props}>
+      <div className="absolute left-[-1px] top-0 h-full w-[1px] bg-gray-300" />
+      {isValidated && (
+        <motion.div
+          initial={{ height: 0 }}
+          animate={{ height: "100%" }}
+          exit={{ height: 0 }}
+          transition={{ duration: 0.5 }}
+          className="absolute left-[-1px] top-0 w-[1px] bg-green-500"
+        />
+      )}
+      {children}
+    </div>
+  );
 };
 
-export const StepHeader = ({ children }: { children: React.ReactNode }) => {
-	const { step: currentStep } = useStep();
-	const { step, toggleActive } = useStepItem();
-	const canAdvance = step < currentStep;
+export const StepHeader = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { step: currentStep } = useStep();
+  const { step, toggleActive } = useStepItem();
+  const canAdvance = step < currentStep;
 
-	return (
-		<button
-			type="button"
-			className={cn("flex flex-row items-center")}
-			disabled={!canAdvance}
-			onClick={() => toggleActive()}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      className={cn("flex flex-row items-center")}
+      disabled={!canAdvance}
+      onClick={() => toggleActive()}>
+      {children}
+    </button>
+  );
 };
 
 export const StepHeaderTitle = ({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) => {
-	return <div className="text-lg font-medium pl-4">{children}</div>;
+  return <div className="pl-4 text-lg font-medium">{children}</div>;
 };
 
-export const StepIcon = ({ children }: { children: React.ReactNode }) => {
-	const { step: currentStep } = useStep();
-	const { step } = useStepItem();
-	const isValidated = currentStep > step;
+export const StepIcon = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { step: currentStep } = useStep();
+  const { step } = useStepItem();
+  const isValidated = currentStep > step;
 
-	return (
-		<div
-			className={cn(
-				"absolute left-[-20px] rounded-full border border-gray-300 bg-white p-2 text-gray-600 transition-all duration-300",
-				isValidated && "border-green-500 text-green-600",
-			)}
-		>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={cn(
+        "absolute left-[-20px] rounded-full border border-gray-300 bg-white p-2 text-gray-600 transition-all duration-300",
+        isValidated && "border-green-500 text-green-600"
+      )}>
+      {children}
+    </div>
+  );
 };
 
 export const StepBody = ({
-	children,
+  children,
 }: {
-	children: ({
-		step,
-		setStep,
-		advance,
-		rewind,
-	}: {
-		step: number;
-		setStep: (step: number) => void;
-		advance: () => void;
-		rewind: () => void;
-	}) => React.ReactNode;
+  children: ({
+    step,
+    setStep,
+    advance,
+    rewind,
+  }: {
+    step: number;
+    setStep: (step: number) => void;
+    advance: () => void;
+    rewind: () => void;
+  }) => React.ReactNode;
 }) => {
-	const { step: currentStep, advance, rewind, setStep } = useStep();
-	const { step, active } = useStepItem();
+  const {
+    step: currentStep,
+    advance,
+    rewind,
+    setStep,
+    shouldAnimate,
+  } = useStep();
+  const { step, active } = useStepItem();
 
-	const isActive = currentStep === step || active;
+  const isActive = currentStep === step || active || !shouldAnimate;
 
-	return (
-		<div className="min-h-2">
-			<AnimatePresence>
-				{isActive && (
-					<motion.div
-						initial={{ opacity: 0, height: 0 }}
-						animate={{ opacity: 1, height: "auto" }}
-						exit={{ opacity: 0, height: 0 }}
-						className="flex flex-col gap-4 overflow-hidden"
-					>
-						{children({ step: currentStep, setStep, advance, rewind })}
-					</motion.div>
-				)}
-			</AnimatePresence>
-		</div>
-	);
+  return (
+    <div className="min-h-2">
+      <AnimatePresence>
+        {isActive && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{
+              duration: shouldAnimate ? 0.3 : 0.5,
+            }}
+            className="flex flex-col gap-4 overflow-hidden">
+            {children({
+              step: currentStep,
+              setStep,
+              advance,
+              rewind,
+            })}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 };
--- console/src/pages/settings/cloud/aws.tsx
+++ console/src/pages/settings/cloud/aws.tsx
@@ -1,173 +1,183 @@
-import { useState } from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { MdChecklist, MdOutlineLocalPolice, MdPreview } from "react-icons/md";
-import { TfiPackage } from "react-icons/tfi";
 import { Page } from "@/components/page-comp/header";
 import { Stepper } from "@/components/stepper";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import {
+  MdChecklist,
+  MdOutlineLocalPolice,
+  MdPreview,
+} from "react-icons/md";
+import { TfiPackage } from "react-icons/tfi";
 
 export const AwsPage = () => {
-	const [awsAccount, setAwsAccount] = useState<string>("");
-	const [agreement, setAgreement] = useState<boolean>(false);
+  const [awsAccount, setAwsAccount] = useState<string>("");
+  const [agreement, setAgreement] = useState<boolean>(false);
 
-	return (
-		<Page.Container>
-			<Page.Header>
-				<Page.Title>Connect your Amazon Web Service account</Page.Title>
-				<Page.Description>
-					Lets authorize Brume Cloud to operate on your AWS account
-				</Page.Description>
-			</Page.Header>
-			<Page.Body className="h-full pt-16">
-				<Stepper.Root>
-					<Stepper.Item>
-						<Stepper.Header>
-							<Stepper.Icon>
-								<TfiPackage className="h-5 w-5" />
-							</Stepper.Icon>
-							<Stepper.Title>Choose the account</Stepper.Title>
-						</Stepper.Header>
-						<Stepper.Body>
-							{({ setStep }) => (
-								<>
-									<div className="flex flex-col space-y-1">
-										<div className="text-sm font-medium">AWS account</div>
-										<p className="text-sm text-gray-500">
-											Enter the Account ID of the one you want to connect
-										</p>
-									</div>
-									<div className="flex flex-row gap-4">
-										<Input
-											placeholder="Enter the Account ID (123456789012)"
-											className="max-w-96"
-											value={awsAccount}
-											onChange={(e) => setAwsAccount(e.target.value)}
-										/>
-										<Button
-											className="w-32"
-											onClick={() => setStep(1)}
-											disabled={awsAccount.length !== 12}
-										>
-											<FaArrowRightLong className="h-4 w-4" />
-											Continue
-										</Button>
-									</div>
-								</>
-							)}
-						</Stepper.Body>
-					</Stepper.Item>
-					<Stepper.Item>
-						<Stepper.Header>
-							<Stepper.Icon>
-								<MdChecklist className="h-5 w-5" />
-							</Stepper.Icon>
-							<Stepper.Title>Agreement</Stepper.Title>
-						</Stepper.Header>
-						<Stepper.Body>
-							{({ setStep }) => {
-								return (
-									<>
-										<div className="flex flex-col space-y-1">
-											<div className="text-sm font-medium">
-												Brume will operate on your account
-											</div>
-											<p className="text-sm text-gray-500">
-												Brume will operate on your account, only managing its
-												ressources. You are responsible for the stack you are
-												deploying.
-											</p>
-										</div>
-										<div className="pt-4">
-											<div className="flex items-center gap-3">
-												<Checkbox
-													id="terms"
-													checked={agreement}
-													onCheckedChange={(checked) => {
-														setAgreement(
-															checked === "indeterminate" ? false : checked,
-														);
-														if (checked) {
-															setStep(2);
-														} else {
-															setStep(1);
-														}
-													}}
-												/>
-												<Label htmlFor="terms">
-													Accept the changes and the risks
-												</Label>
-											</div>
-										</div>
-									</>
-								);
-							}}
-						</Stepper.Body>
-					</Stepper.Item>
-					<Stepper.Item>
-						<Stepper.Header>
-							<Stepper.Icon>
-								<MdOutlineLocalPolice className="h-5 w-5" />
-							</Stepper.Icon>
-							<Stepper.Title>Authorizing Brume Cloud</Stepper.Title>
-						</Stepper.Header>
-						<Stepper.Body>
-							{({ setStep }) => {
-								return (
-									<>
-										<div className="flex flex-col space-y-1">
-											<div className="text-sm font-medium">
-												Creating the Assume role
-											</div>
-											<p className="text-sm text-gray-500">
-												Brume will use an assumed role on your account to manage
-												the ressources it deploy.
-											</p>
-										</div>
-										<div className="pt-4">
-											<Button onClick={() => setStep(3)}>
-												Create the role
-											</Button>
-										</div>
-									</>
-								);
-							}}
-						</Stepper.Body>
-					</Stepper.Item>
-					<Stepper.Item className="h-full">
-						<Stepper.Header>
-							<Stepper.Icon>
-								<MdPreview className="h-5 w-5" />
-							</Stepper.Icon>
-							<Stepper.Title>Verification</Stepper.Title>
-						</Stepper.Header>
-						<Stepper.Body>
-							{({ setStep }) => {
-								return (
-									<>
-										<div className="flex flex-col space-y-1">
-											<div className="text-sm font-medium">
-												Deploying the end to end test stack
-											</div>
-											<p className="text-sm text-gray-500">
-												Brume will deploy a very simple stack to verify
-												everything is working
-											</p>
-										</div>
-										<div className="pt-4">
-											<Button onClick={() => setStep(4)}>
-												Start the end to end test
-											</Button>
-										</div>
-									</>
-								);
-							}}
-						</Stepper.Body>
-					</Stepper.Item>
-				</Stepper.Root>
-			</Page.Body>
-		</Page.Container>
-	);
+  return (
+    <Page.Container>
+      <Page.Header>
+        <Page.Title>
+          Connect your Amazon Web Service account
+        </Page.Title>
+        <Page.Description>
+          Lets authorize Brume Cloud to operate on your AWS account
+        </Page.Description>
+      </Page.Header>
+      <Page.Body className="h-full pt-16">
+        <Stepper.Root shouldAnimate>
+          <Stepper.Item>
+            <Stepper.Header>
+              <Stepper.Icon>
+                <TfiPackage className="h-5 w-5" />
+              </Stepper.Icon>
+              <Stepper.Title>Choose the account</Stepper.Title>
+            </Stepper.Header>
+            <Stepper.Body>
+              {({ setStep }) => (
+                <>
+                  <div className="flex flex-col space-y-1">
+                    <div className="text-sm font-medium">
+                      AWS account
+                    </div>
+                    <p className="text-sm text-gray-500">
+                      Enter the Account ID of the one you want to
+                      connect
+                    </p>
+                  </div>
+                  <div className="flex flex-row gap-4">
+                    <Input
+                      placeholder="Enter the Account ID (123456789012)"
+                      className="max-w-96"
+                      value={awsAccount}
+                      onChange={(e) => setAwsAccount(e.target.value)}
+                    />
+                    <Button
+                      className="w-32"
+                      onClick={() => setStep(1)}
+                      disabled={awsAccount.length !== 12}>
+                      <FaArrowRightLong className="h-4 w-4" />
+                      Continue
+                    </Button>
+                  </div>
+                </>
+              )}
+            </Stepper.Body>
+          </Stepper.Item>
+          <Stepper.Item>
+            <Stepper.Header>
+              <Stepper.Icon>
+                <MdChecklist className="h-5 w-5" />
+              </Stepper.Icon>
+              <Stepper.Title>Agreement</Stepper.Title>
+            </Stepper.Header>
+            <Stepper.Body>
+              {({ setStep }) => {
+                return (
+                  <>
+                    <div className="flex flex-col space-y-1">
+                      <div className="text-sm font-medium">
+                        Brume will operate on your account
+                      </div>
+                      <p className="text-sm text-gray-500">
+                        Brume will operate on your account, only
+                        managing its ressources. You are responsible
+                        for the stack you are deploying.
+                      </p>
+                    </div>
+                    <div className="pt-4">
+                      <div className="flex items-center gap-3">
+                        <Checkbox
+                          id="terms"
+                          checked={agreement}
+                          onCheckedChange={(checked) => {
+                            setAgreement(
+                              checked === "indeterminate"
+                                ? false
+                                : checked
+                            );
+                            if (checked) {
+                              setStep(2);
+                            } else {
+                              setStep(1);
+                            }
+                          }}
+                        />
+                        <Label htmlFor="terms">
+                          Accept the changes and the risks
+                        </Label>
+                      </div>
+                    </div>
+                  </>
+                );
+              }}
+            </Stepper.Body>
+          </Stepper.Item>
+          <Stepper.Item>
+            <Stepper.Header>
+              <Stepper.Icon>
+                <MdOutlineLocalPolice className="h-5 w-5" />
+              </Stepper.Icon>
+              <Stepper.Title>Authorizing Brume Cloud</Stepper.Title>
+            </Stepper.Header>
+            <Stepper.Body>
+              {({ setStep }) => {
+                return (
+                  <>
+                    <div className="flex flex-col space-y-1">
+                      <div className="text-sm font-medium">
+                        Creating the Assume role
+                      </div>
+                      <p className="text-sm text-gray-500">
+                        Brume will use an assumed role on your account
+                        to manage the ressources it deploy.
+                      </p>
+                    </div>
+                    <div className="pt-4">
+                      <Button onClick={() => setStep(3)}>
+                        Create the role
+                      </Button>
+                    </div>
+                  </>
+                );
+              }}
+            </Stepper.Body>
+          </Stepper.Item>
+          <Stepper.Item className="h-full">
+            <Stepper.Header>
+              <Stepper.Icon>
+                <MdPreview className="h-5 w-5" />
+              </Stepper.Icon>
+              <Stepper.Title>Verification</Stepper.Title>
+            </Stepper.Header>
+            <Stepper.Body>
+              {({ setStep }) => {
+                return (
+                  <>
+                    <div className="flex flex-col space-y-1">
+                      <div className="text-sm font-medium">
+                        Deploying the end to end test stack
+                      </div>
+                      <p className="text-sm text-gray-500">
+                        Brume will deploy a very simple stack to
+                        verify everything is working
+                      </p>
+                    </div>
+                    <div className="pt-4">
+                      <Button onClick={() => setStep(4)}>
+                        Start the end to end test
+                      </Button>
+                    </div>
+                  </>
+                );
+              }}
+            </Stepper.Body>
+          </Stepper.Item>
+        </Stepper.Root>
+      </Page.Body>
+    </Page.Container>
+  );
 };
